4c866ff8d78d997f8bcb91f79bd428947c5b4dbc
Tatyana Gerasina <gtstata@gmail.com>
2024-01-31 22:47:29 +0300

    Created test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIfMore() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
